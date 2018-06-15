1433d5dbdb19b60a10cc00957b01e98a344b6430
tudd4711@gmail.com <tudd4711@gmail.com>
2018-06-15 16:40:50 +0700

    First Commit

---
+++ EzAutoResizingTableCell/CustomTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  CustomTableViewCell.swift
+//  EzAutoResizingTableCell
+//
+//  Created by MacMiniCorei5-26Ghz on 6/15/18.
+//  Copyright © 2018 GVN. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var bodyLabel: UILabel!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ EzAutoResizingTableCell/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  EzAutoResizingTableCell
+//
+//  Created by MacMiniCorei5-26Ghz on 6/15/18.
+//  Copyright © 2018 GVN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var customTable: UITableView!
+    @IBOutlet weak var sizeSlider: UISlider!
+    
+    var cellList = [CustomTableViewCell]()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        customTable.delegate = self
+        customTable.dataSource = self
+        // Do any additional setup after loading the view, typically from a nib.
+        customTable.register(UINib(nibName: "CustomTableViewCell", bundle: nil), forCellReuseIdentifier: "cell")
+        customTable.rowHeight = UITableViewAutomaticDimension
+        customTable.estimatedRowHeight = 100
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onValueChanged(_ sender: UISlider) {
+        customTable.beginUpdates()
+        for cell in cellList {
+            cell.bodyLabel.font = UIFont.systemFont(ofSize: CGFloat(sender.value))
+            cell.headerLabel.font = UIFont.systemFont(ofSize: CGFloat(sender.value))
+        }
+        customTable.endUpdates()
+    }
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    
+}
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = customTable.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
+        cell.headerLabel.text = "Angela K. Snyder"
+        cell.bodyLabel.text = "Ap #198-1768 Metus. Rd."
+        cell.headerLabel.lineBreakMode = .byWordWrapping
+        cell.headerLabel.sizeToFit()
+        cell.headerLabel.layoutIfNeeded()
+        cell.bodyLabel.lineBreakMode = .byWordWrapping
+        cell.bodyLabel.sizeToFit()
+        cell.bodyLabel.layoutIfNeeded()
+        cellList.append(cell)
+        return cell
+    }
+}
+
